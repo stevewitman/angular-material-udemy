d6b24da3e6827bae08de18c5cf974a0f10bd761f
stevewitman <stevewitman@gmail.com>
2019-01-02 23:40:03 -0700

    Add angular flex layout and set up template driven signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -27,8 +29,10 @@ import { AppRoutingModule } from './app-routing.module'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+  } from '@angular/material';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     MatButtonModule,
-
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     MatButtonModule,
-
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
